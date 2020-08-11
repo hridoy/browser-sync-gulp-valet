414be0534ab6a7041684cccfe59a3cda2893955e
Hridoy <ac@hridoy.net>
2020-08-12 04:47:37 +0530

    default configuration

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+const gulp = require("gulp");
+const browserSync = require("browser-sync").create();
+const siteName = '1337x'; // set your siteName here
+const userName = 'anshika'; // set your macOS userName here
+
+// Set assets paths.
+var paths = {
+    php: [ '*.php', '**/*.php' ],
+    scripts: [ 'js/*.js' ],
+    styles: [ '*.css', 'css/*.css' ]
+};
+
+/**
+ * Reload browser after PHP & JS file changes and inject CSS changes.
+ *
+ * https://browsersync.io/docs/gulp
+ */
+gulp.task( 'default', function() {
+    browserSync.init({
+        proxy: 'https://' + siteName + '.test',
+        host: siteName + '.test',
+        open: 'external',
+        port: 8000,
+        https: {
+            key:
+                '/Users/' +
+                userName +
+                '/.config/valet/Certificates/' +
+                siteName +
+                '.test.key',
+            cert:
+                '/Users/' +
+                userName +
+                '/.config/valet/Certificates/' +
+                siteName +
+                '.test.crt'
+        }
+    });
+
+    gulp.watch( paths.php ).on( 'change', browserSync.reload );
+    gulp.watch( paths.scripts ).on( 'change', browserSync.reload );
+
+    gulp.watch( paths.styles, function() {
+        gulp.src( paths.styles ).pipe( browserSync.stream() );
+    });
+});
---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Browsersync Php Valet default configuration</title>
+</head>
+<body>
+    
+</body>
+</html>
